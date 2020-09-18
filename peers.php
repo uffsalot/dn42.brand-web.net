8facdedd7392c21b934e979f49f7337dd1e48c3b
Jan Brand <jbrand@merentis.com>
2020-09-18 08:47:40 +0200

    swap ipv4 and ipv6 table

--- peers.php
+++ peers.php
@@ -3,41 +3,6 @@
     <div class="row">
         <div class="col-md-6">
         <div class="card text-white bg-dark mb-3 h-100">
-            <div class="card-header">BGP IPv4 Peers</div>
-            <div class="card-body">
-                <table class="table table-hover">
-                    <thead>
-			<tr>
-			<th scope="col" style="width: 0.133333%;">&nbsp;</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">AS</th>
-                        <th scope="col">Last state changed</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">State</th>
-                        </tr>
-                    </thead>
-                <tbody>
-                    <?php require_once("inc/get_peers.php"); 
-                    if($resultv4){
-                        foreach($protocolsv4 as $peer){
-    			    echo '<tr>';
-			    if($peer['state'] == "up") {echo '<td class="table-success"></td>';}else{echo '<td class="table-danger"></td>';}
-                            echo '<th scope="row">'.$peer['protocol'].'</th>';
-			    echo '<td><a href="https://explorer.burble.com/#/aut-num/AS'.$peer['neighbor_as'].'">'.$peer['neighbor_as'].'</a></td>';
-			    echo '<td>'.$peer['state_changed'].'</td>';
-                            echo '<td>'.$peer['state'].'</td>';
-                            echo '<td>'.$peer['connection'].'</td></tr>';
-                        }
-                    } else {
-                        echo "Birdwatcher currently not available";}
-                        ?>
-                </tbody> 
-            </table> 
-            </div>
-        </div>
-        </div>
-        <div class="col-md-6">
-        <div class="card text-white bg-dark mb-3 h-100">
             <div class="card-header">BGP IPv6 Peers</div>
             <div class="card-body">
                 <table class="table table-hover">
@@ -60,6 +25,41 @@
                             echo '<th scope="row">'.$peer['protocol'].'</th>';
 			    echo '<td><a href="https://explorer.burble.com/#/aut-num/AS'.$peer['neighbor_as'].'">'.$peer['neighbor_as'].'</a></td>';
                             echo '<td>'.$peer['state_changed'].'</td>';
+                            echo '<td>'.$peer['state'].'</td>';
+                            echo '<td>'.$peer['connection'].'</td></tr>';
+                        }
+                    } else {
+                        echo "Birdwatcher currently not available";}
+                        ?>
+                </tbody> 
+            </table> 
+            </div>
+        </div>
+        </div>
+        <div class="col-md-6">
+        <div class="card text-white bg-dark mb-3 h-100">
+            <div class="card-header">BGP IPv4 Peers</div>
+            <div class="card-body">
+                <table class="table table-hover">
+                    <thead>
+			<tr>
+			<th scope="col" style="width: 0.133333%;">&nbsp;</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">AS</th>
+                        <th scope="col">Last state changed</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">State</th>
+                        </tr>
+                    </thead>
+                <tbody>
+                    <?php require_once("inc/get_peers.php"); 
+                    if($resultv4){
+                        foreach($protocolsv4 as $peer){
+    			    echo '<tr>';
+			    if($peer['state'] == "up") {echo '<td class="table-success"></td>';}else{echo '<td class="table-danger"></td>';}
+                            echo '<th scope="row">'.$peer['protocol'].'</th>';
+			    echo '<td><a href="https://explorer.burble.com/#/aut-num/AS'.$peer['neighbor_as'].'">'.$peer['neighbor_as'].'</a></td>';
+			    echo '<td>'.$peer['state_changed'].'</td>';
                             echo '<td>'.$peer['state'].'</td>';
                             echo '<td>'.$peer['connection'].'</td></tr>';
                         }
