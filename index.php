cda367a9916bda85e36f84f4489fd391572432ef
Jan Brand <jan+git@brand-web.net>
2020-10-21 22:16:25 +0200

    peering information extra file

--- index.php
+++ index.php
@@ -22,35 +22,7 @@
             </div>
         </div>
         </div>
-        <div class="col-md-6">
-        <div class="card text-white bg-dark mb-3 h-100">
-            <div class="card-header">Peering</div>
-            <div class="card-body">
-                <dl>
-                    <dt>Location</dt>
-                    <dd>Frankfurt, Germany - IP-Projects (First Colo FRA4)</dd>
-                    <dt>Bandwith</dt>
-                    <dd>1 Gbps</dd>
-                    <dt>Remote (IPv4/IPv6)</dt>
-                    <dd>dn42-de-fra4.brand-web.net</dd>
-                    <dt>AS</dt>
-                    <dd>4242420780</dd>
-                    <dt>BGP Endpoint (IPv4)</dt>
-                    <dd>172.20.191.129</dd>
-                    <dt>BGP Endpoint (IPv6)</dt>
-                    <dd>fe80::780</dd>
-                    <dt>BGP Multiprotocol</dt>
-                    <dd>not yet</dd>
-                    <dt>VPN Type</dt>
-                    <dd>Wireguard (preferred), OpenVPN</dd>
-                    <dt>Wireguard Port</dt>
-                    <dd>4&lt;last 4 digits of your ASN&gt;</dd>
-                    <dt>Wireguard Public Key</dt>
-                    <dd>7V65FxvD9AQetyUr0qSiu+ik8samB4Atrw2ekvC0xQM=</dd>
-                </dl>
-            </div>
-        </div>
-        </div>
+        <?php require_once('inc/peering.inc.php');?>
     </div>
 </div>
 <script src="js/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
